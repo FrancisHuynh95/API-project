4702da6dbc8ea74ced5a3664f3fbf0b53b1599ed
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-03-16 11:09:25 -0700

    completed phase 3

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,23 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+// GET /api/restore-user
+const { restoreUser } = require('../../utils/auth.js');
+
+router.use(restoreUser);
+
+
+
+// ...
+
+// GET /api/require-auth
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 module.exports = router;
