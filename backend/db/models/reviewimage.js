553ad466ad8e1839f705bb52c20fb9d2eac9d383
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-03-21 09:44:23 -0700

    potentially fixed pt2

--- backend/db/models/reviewimage.js
+++ backend/db/models/reviewimage.js
@@ -23,7 +23,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'reviews'
+        model: 'Reviews'
       }
     }
   }, {
