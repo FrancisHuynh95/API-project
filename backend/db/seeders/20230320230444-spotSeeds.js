28f84bab0b0cef588b9dfb78d31069454645370f
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-04-16 20:38:16 -0700

    fixed seeders

--- backend/db/seeders/20230320230444-spotSeeds.js
+++ backend/db/seeders/20230320230444-spotSeeds.js
@@ -19,7 +19,7 @@ module.exports = {
         lat: 23,
         lng: 123,
         name: 'Big House In San Francisco',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vulputate tortor vel ligula aliquet ornare. Maecenas eu nulla tellus. Duis rhoncus, arcu ac suscipit maximus, ipsum lectus pharetra tortor, vel faucibus tellus ante eu metus. Nam pellentesque ullamcorper molestie. Quisque viverra libero sed gravida ornare. Praesent sit amet ornare odio. Fusce scelerisque a lorem a fringilla. Quisque tellus nisl, auctor ac ligula eu, posuere bibendum quam. Pellentesque laoreet neque vel malesuada fringilla. Duis id luctus nisi. Aliquam lorem velit, blandit dapibus ex non, pretium euismod dui. Integer est ligula, iaculis ac porta at, luctus eget purus. Duis quis dignissim turpis.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vulputate tortor vel ligula aliquet ornare. Maecenas eu nulla tellus. Duis rhoncus, arcu ac suscipit maximus, ipsum lectus pharetra tortor, vel faucibus tellus ante eu metus. ',
         price: 9.99,
         ownerId: 1
       },
@@ -31,7 +31,7 @@ module.exports = {
         lat: 12,
         lng: 123,
         name: 'House in Sacramento',
-        description: 'Vivamus ut lacus lobortis, varius metus non, pellentesque augue. Sed eget ipsum tempor, aliquam quam in, auctor nunc. Vivamus consectetur feugiat purus, ac pellentesque tellus gravida at. Phasellus varius rhoncus risus vel aliquet. Maecenas consectetur massa placerat, egestas mi ullamcorper, volutpat lorem. Donec convallis dictum elit, ut vulputate urna lacinia nec. Donec tincidunt est sed mauris vehicula laoreet. Vestibulum aliquet orci at turpis convallis blandit. Quisque consequat finibus urna, ac ornare orci malesuada a. Mauris venenatis erat sed sem pharetra tempor. Sed justo eros, malesuada id scelerisque vitae, pharetra non tellus. Ut malesuada dolor quis sem dictum, id congue nibh consectetur. In sodales justo vitae tortor viverra, vitae lobortis eros elementum. Vivamus placerat tempus orci. Nullam nec suscipit enim. Sed vel dapibus dui.',
+        description: 'Vivamus ut lacus lobortis, varius metus non, pellentesque augue. Sed eget ipsum tempor, aliquam quam in, auctor nunc. Vivamus consectetur feugiat purus, ac pellentesque tellus gravida at.',
         price: 19.99,
         ownerId: 2
       },
@@ -43,7 +43,7 @@ module.exports = {
         lat: 12,
         lng: 123,
         name: 'House In San Jose',
-        description: 'Quisque lacus neque, bibendum non consequat quis, ornare a ex. Fusce fringilla sed est non finibus. Donec in pharetra quam. Quisque venenatis vel lacus nec fermentum. Donec porta ultrices massa volutpat eleifend. Mauris rutrum suscipit nunc ut facilisis. Donec quis euismod mi, et vulputate lacus. Nullam dictum, lacus eu fringilla auctor, tellus sapien ultrices massa, at feugiat metus neque sit amet nulla. Cras eu dolor dignissim, accumsan libero ut, cursus felis. Vivamus tempor, mi vitae feugiat imperdiet, mi est fringilla eros, at lacinia massa eros id libero. Fusce placerat faucibus odio sollicitudin faucibus. Mauris suscipit sem quis est mollis, in consectetur urna suscipit. Duis varius aliquet ante, a pellentesque turpis egestas luctus. Phasellus consectetur, quam sed mollis euismod, nunc diam pharetra ligula, a egestas dolor erat at diam.',
+        description: 'Quisque lacus neque, bibendum non consequat quis, ornare a ex. Fusce fringilla sed est non finibus. Donec in pharetra quam. Quisque venenatis vel lacus nec fermentum. Donec porta ultrices massa volutpat eleifend.',
         price: 29.99,
         ownerId: 3
       },
