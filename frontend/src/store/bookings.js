184f4c3c967ef0c3fe7a686d9f54c44d830e022d
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-06-19 16:41:46 -0700

    added more to the bookings store

--- frontend/src/store/bookings.js
+++ frontend/src/store/bookings.js
@@ -1,10 +1,107 @@
 import { csrfFetch } from "./csrf";
+const GET_BOOKING = 'GETBOOKING'
+const DELETE_BOOKING = 'DELETEBOOKING'
+const USER_BOOKINGS = 'USERBOOKINGS'
+
+const getBooking = (booking) => {
+    return {
+        type: GET_BOOKING,
+        booking
+    }
+}
+
+const deleteBooking = (booking) => {
+    return {
+        type: DELETE_BOOKING,
+        booking
+    }
+}
+
+const userBookings = (bookings) => {
+    return {
+        type: USER_BOOKINGS,
+        bookings
+    }
+}
+
+export const getBookingThunk = (spotId) => async (dispatch) => {
+    const res = fetch(`/api/spots/${spotId}/bookings`)
+    if(res.ok){
+        const response = await res.json()
+        dispatch(getBooking(response))
+        return response
+    }
+}
+
+export const createBookingThunk = (spotId, booking) => async (dispatch) => {
+    const res = csrfFetch(`/api/spots/${spotId}/bookings`, {
+        method: "POST",
+        headers: {'Content-Type': "application/json"},
+        body: JSON.stringify(booking)
+    })
+    if(res.ok){
+        const response = await res.json()
+        dispatch(getBooking(response))
+        return response
+    }
+}
+
+export const editBookingThunk = (booking, ) => async (dispatch) => {
+    const res = csrfFetch(`/api/bookings/${booking.id}`, {
+        method: "PUT",
+        headers: {'Content-Type': "application/json"},
+        body: JSON.stringify(booking)
+    })
+    if(res.ok){
+        const response = await res.json()
+        dispatch(getBooking(response))
+        return response
+    }
+}
+
+export const removeBookingThunk = (bookingId) => async(dispatch) => {
+    const res = csrfFetch(`/api/bookings/${bookingId}`, {
+        method: 'DELETE'
+    })
+    if(res.ok){
+        const response = await dispatch(deleteBooking(bookingId))
+    }
+}
+
+export const getUserBookingsThunk = () => async (dispatch) => {
+    const res = csrfFetch(`/api/bookings/current`)
+    if(res.ok){
+        const response = await res.json()
+        await dispatch(userBookings(response))
+        return response
+    }
+}
 
 
-const initalState = {user: {}, spot: {}}
+const initalState = {}
 const bookingReducer = (state = initalState, action) => {
+    let newState
     switch(action.type){
-        default:
+        case GET_BOOKING:{
+            newState = {}
+            action.booking.forEach(booking =>
+                newState[booking.id] = booking
+            )
+            return newState;
+        }
+        case USER_BOOKINGS:{
+            newState = {}
+            action.bookings.forEach(booking =>
+                newState[booking.id] = booking
+            )
+            return newState;
+        }
+        case DELETE_BOOKING: {
+            newState = {...state}
+            const res = newState.filter(booking => booking.id !== action.booking)
+            return res;
+        }
+            default:
             return state;
     }
 }
