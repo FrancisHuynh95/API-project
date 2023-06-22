2da3d49abff326035c0abf35fda808764c540f18
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-06-22 14:24:19 -0700

    edit booking modal

--- frontend/src/components/bookingCard/index.js
+++ frontend/src/components/bookingCard/index.js
@@ -1,11 +1,16 @@
 import DeleteBooking from "../deleteBooking"
 import OpenModalButton from "../OpenModalButton"
+import UpdateBooking from "../updateBooking"
 
 function BookingCard({booking}){
     return (
         <div className="bookingSpotCard" title={`${booking.Spot?.name}`}>
         <img src={`${booking.Spot?.previewImage}`}></img>
         <p>${`${booking.Spot?.price}`}</p>
+        < OpenModalButton
+        buttonText={"Update"}
+        modalComponent={<UpdateBooking booking={booking}/>}
+         />
         < OpenModalButton
         buttonText={"Delete"}
         modalComponent={<DeleteBooking bookingId={booking.id} endDate={booking.endDate}/>}
---
+++ frontend/src/components/updateBooking/index.js
@@ -0,0 +1,9 @@
+function UpdateBooking({booking}) {
+    return (
+        <div className="updateBookingModal">
+            <h1>Update Booking</h1>
+        </div>
+    )
+}
+
+export default UpdateBooking
