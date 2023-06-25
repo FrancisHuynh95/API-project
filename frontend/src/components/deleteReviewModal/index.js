5ef2a97deb1703f3b041b89dd565979cb7698eed
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-06-25 16:53:29 -0700

    delete review modal

--- frontend/src/components/deleteReviewModal/index.js
+++ frontend/src/components/deleteReviewModal/index.js
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux"
 import { useModal } from "../../context/Modal"
 import {deleteReviewThunk} from "../../store/review"
+import './deleteReview.css'
 
 function DeleteReview({review}) {
     const dispatch = useDispatch()
@@ -16,8 +17,10 @@ function DeleteReview({review}) {
         <div className="DeleteReviewModal">
             <h1>Delete Review</h1>
             <p>Are you sure you want to delete this review?</p>
+            <div className="deleteReviewButtons">
             <button onClick={() => handleDelete()}>Delete</button>
             <button onClick={() => closeModal()}>Cancel</button>
+            </div>
         </div>
     )
 }
